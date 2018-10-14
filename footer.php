1c1a8b1a250c528042d3d9e82d755c53b96db0ea
kerrywall <kerry.wall@gmail.com>
2018-10-14 12:36:27 -0400

    initial footer styling

--- footer.php
+++ footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'min' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'min' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'min' ), 'min', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p class="site-title"><?php bloginfo( 'name' ); ?></p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
