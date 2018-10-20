5d81d196ec985d1848ab8b945fccf7a17742a1ef
kerrywall <kerry.wall@gmail.com>
2018-10-19 21:54:29 -0400

    adding copyright info

--- footer.php
+++ footer.php
@@ -16,6 +16,7 @@
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
 			<p class="site-title"><?php bloginfo( 'name' ); ?></p>
+			<p class="copyright">&copy; 2018</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
